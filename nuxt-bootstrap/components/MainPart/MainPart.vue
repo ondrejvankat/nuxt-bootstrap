9e4f6c01481bfc3bef9b79351155257566318e63
ondrejvankat <ondrej.vankat@peckadesing.cz>
2022-06-10 21:59:07 +0200

    Testovací aplikace - přidání vypsání youtube videí dle odkazu v poli

--- nuxt-bootstrap/components/MainPart/MainPart.vue
+++ nuxt-bootstrap/components/MainPart/MainPart.vue
@@ -4,7 +4,7 @@
     <section class="py-5 text-center container">
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light">Testovací web</h1>
+          <h1 class="fw-light">Youtube videa</h1>
         </div>
       </div>
     </section>
@@ -13,7 +13,12 @@
       <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <youtube-card/>
+        <youtube-card
+          v-for = "(link, index) of links"
+          :key = "(index + 2)"
+          :link = "link"
+          :link-to-image = linksToImage[index]
+        />
         </div>
       </div>
     </div>
@@ -26,6 +31,20 @@ import YoutubeCard from "~/components/MainPart/YoutubeCard"
 export default {
   components: {
     YoutubeCard
+  },
+  data () {
+    return {
+      links: [
+        'https://www.youtube.com/watch?v=E1koDpTt9ro',
+        'https://www.youtube.com/watch?v=60ItHLz5WEA',
+        'https://www.youtube.com/watch?v=kXYiU_JCYtU'
+      ],
+      linksToImage: [
+        'https://img.youtube.com/vi/E1koDpTt9ro/0.jpg',
+        'https://img.youtube.com/vi/60ItHLz5WEA/0.jpg',
+        'https://img.youtube.com/vi/kXYiU_JCYtU/0.jpg'
+      ]
+    }
   }
 
 }
--- nuxt-bootstrap/components/MainPart/YoutubeCard.vue
+++ nuxt-bootstrap/components/MainPart/YoutubeCard.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="col">
     <div class="card shadow-sm">
-      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+      <img :src="linkToImage"/>
 
       <div class="card-body">
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">Pro otevření klikněte na "View"</p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+            <button type="button" @click="onClick" class="btn btn-sm btn-outline-secondary">View</button>
           </div>
-          <small class="text-muted">9 mins</small>
+          <small class="text-muted">Youtube</small>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+
+export default {
+  props: {
+    link:{
+      required: true,
+      type: String
+    },
+    linkToImage:{
+      required: true,
+      type: String
+    }
+  },
+  methods: {
+    onClick (){
+    window.open(this.link);
+    }
+  }
+}
+
+</script>
